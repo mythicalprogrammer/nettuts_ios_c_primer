4fef73cebfc3e4395309bde168c318dcea986537
Anthony Doan <anthonydoan@android_a100001784f692.gateway.2wire.net>
2013-07-05 05:41:46 -0700

    done with the c primer

--- C Primer/main.c
+++ C Primer/main.c
@@ -8,11 +8,31 @@
 
 #include <stdio.h>
 
+int multiplyByFive(int a); // function prototype
+
 int main(int argc, const char * argv[])
 {
 
     // insert code here...
     printf("Hello, World!\n");
+    // variables
+    int a = 5;
+    float b = 6.3;
+    char c = 'r';
+    printf("this is an int: %i\n", a);
+    printf("this is a float: %f\n", b);
+    printf("this is a char: %c\n", c);
+    // Functions
+    printf("Five multiplied by five is %i\n", multiplyByFive(5));
+    // Pointers
+    int d = 5;
+    int *e = &d;
+    printf("d has a value of %i\n", d);
+    printf("e has a value of %p\n", e);
+    printf("the object that e points to has a value of %i\n", *e);
     return 0;
 }
 
+int multiplyByFive(int a) {
+    return a * 5;
+}
