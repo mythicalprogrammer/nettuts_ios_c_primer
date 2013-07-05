c24a64994dfa1ec11dcedd5a669700cee8660eb3
Anthony Doan <anthonydoan@android_a100001784f692.gateway.2wire.net>
2013-07-05 05:16:24 -0700

    Initial Commit

---
+++ C Primer/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  C Primer
+//
+//  Created by Anthony Doan on 7/5/13.
+//  Copyright (c) 2013 Mobile Tuts. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
